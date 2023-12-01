8896e5548fc26c1569d835367af0531327e50e3f
r88amj <ricardo.88amj@gmail.com>
2023-12-01 15:48:16 -0600

    Seccion de botes
    Reponsive y consumiendo la API

---
+++ src/components/BoatsCards.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react'
+import BoatItem from './BoatItem'
+import './BoatsCards.css'
+
+function BoatsCards() {
+    const [boats, setBoats] = useState([])
+
+    const urlBoatsInitial = "https://marinero.mx/api/boat/search"
+
+    const fetchBoats = () =>{
+        fetch(urlBoatsInitial)
+            .then(response => response.json())
+            .then(data => setBoats(data.data))
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchBoats(urlBoatsInitial)
+    }, [])
+
+  return (
+    <>
+      <div className="cardsBoatsCs">
+        <div className="container">
+          <h1 className='text-center title__Section'>EMBARCACIONES</h1>
+          <h4 className='text-center mb-4 subtitle__Section'>¡Selección destacada por la comunidad!</h4>
+          <BoatItem boatItem={boats} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BoatsCards;
