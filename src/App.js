ae1f541b4d3ac7028c86f34dd73bfd4fd1d6be2c
Ricardo May Jiménez <57809006+r88amj@users.noreply.github.com>
2023-12-08 16:05:13 -0600

    login
    es una demo del login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-import Home from './components/pages/Home'
-import NavigationBar from './components/NavigationBar/NavigationBar';
+import Home from './components/pages/Home';
+import  Navbar  from './components/Navbar/Navbar'
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
 
 function App() {
   return (
     <>
       <Router>
-        <NavigationBar/>
+        <Navbar />
+       {/* <NavigationBar/>*/}
         <Routes>
           <Route path='/' exact Component={Home}/>
+          <Route path='/login' exact Component={LoginPage}/>
+          <Route path='/register' exact Component={RegisterPage} />
         </Routes>
       </Router>
     </>
---
+++ src/utils/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbAoTwpqJ7PtX7NHRlTVGy__uVkQrEKIo",
+  authDomain: "marineroweb-551e9.firebaseapp.com",
+  projectId: "marineroweb-551e9",
+  storageBucket: "marineroweb-551e9.appspot.com",
+  messagingSenderId: "496096701623",
+  appId: "1:496096701623:web:519e3d032fc1fd4e59b132",
+  measurementId: "G-50K3BKFY65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export {app, auth};
