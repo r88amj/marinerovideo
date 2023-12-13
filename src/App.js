e4500c240542bf0948cc030077be2f4352a04c09
r88amj <ricardo.88amj@gmail.com>
2023-12-13 10:27:43 -0600

    update-1312
    Modal inicio de sesion y registro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Home from './components/pages/Home';
-import  Navbar  from './components/Navbar/Navbar'
 import LoginPage from './components/pages/LoginPage';
 import RegisterPage from './components/pages/RegisterPage';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-       {/* <NavigationBar/>*/}
+       <NavigationBar/>
         <Routes>
           <Route path='/' exact Component={Home}/>
           <Route path='/login' exact Component={LoginPage}/>
--- src/components/ModalLogin/ModalLogin.js
+++ src/components/ModalLogin/ModalLogin.js
@@ -1,86 +1,133 @@
-import React, {useState} from 'react'
-import { Modal } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Form from 'react-bootstrap/Form';
-import Image from 'react-bootstrap/Image';
-import './ModalLogin.css'
-import ModalRegister from '../ModalResgister/ModalRegister';
+import React, { useState } from 'react';
+import { Modal, Tab, Tabs, Image, Col, Row, Form, Button } from 'react-bootstrap';
+import './ModalLogin.css';
+import LoginImg from '../Assets/login.jpg'
+import RegisterImg from '../Assets/register.jpg'
 
-function ModalLogin ({show, handleClose}) {
-  const [showRegister, setShowRegister] = useState(false);
+function ModalLogin({ show, handleClose }) {
 
-  const handleCloseRegister = () => setShowRegister(false);
-  const handleShowRegister = () => {
-    setShowRegister(true);
-    handleClose();
-  };
   return (
     <>
-    <Modal
-      show={show}
-      onHide={handleClose}
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Body>
-        <Row className="modal__login">
-          <Col md={6} className="imgRight">
-            <Image
-              src="https://images.pexels.com/photos/8917662/pexels-photo-8917662.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              fluid
-              alt='login_img'
-            />
-          </Col>
-          <Col md={6} className='form__register'>
-            <Modal.Title className="modal-header">
-              <button className="btn--primary closeButton" onClick={handleClose}>
-                X
-              </button>
-            </Modal.Title>
-            <Form className="text-center">
-              <Form.Label className="mb-3">INICIAR SESIÓN</Form.Label>
-            </Form>
-            <Form className="mx-auto">
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control 
-                type="email"
-                name='email' 
-                placeholder="Correo Electrónico" />
-              </Form.Group>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className='ModalLoginRegister'
+      >
+        <Modal.Title className="modal-header">
+          <button className="btn--primary closeButton" onClick={handleClose}>
+            X
+          </button>
+        </Modal.Title>
+        <Modal.Body>
+          <Tabs
+            defaultActiveKey="login"
+            id="uncontrolled-tab-example"
+            className="justify-content-center"
+          >
+            <Tab eventKey="login" title="Iniciar sesión">
+              <Row>
+                <Col md={6} className="colImg">
+                  <img src={LoginImg} fluid />
+                </Col>
+                <Col md={6} className="formLogin">
+                  <Form>
+                    {/* Aquí va tu formulario de inicio de sesión */}
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Correo Electrónico</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="Correo Electrónico"
+                      />
+                    </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Control 
-                type="password" 
-                name='password'
-                placeholder="Contraseña" />
-              </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Contraseña</Form.Label>
+                      <Form.Control type="password" placeholder="Contraseña" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Recordarme" />
+                    </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Recordarme" />
-              </Form.Group>
-              <Form className="text-center">
-                <Button variant="primary" onClick={handleClose}>
-                  Iniciar Sesión
-                </Button>
-              </Form>
-              <Form className='pt-3 pb-2 text-right'>
-                <a href='https://marinero.mx/forgot-password' target='blank'>¿Has olvidado tu contraseña?</a>
-              </Form>
-            </Form>
-          </Col>
-        </Row>
-      </Modal.Body>
-      <Modal.Footer>
-        <p>
-          ¿No tiene una cuenta?<button className='btn--primary pl-2' onClick={handleShowRegister}> Registrarse</button>
-        </p>
-      </Modal.Footer>
-    </Modal>
-    <ModalRegister showRegister={showRegister} handleCloseRegister={handleCloseRegister}/>
+                    <Form.Group controlId="formForgotPassword">
+                      <Form.Text className="text-right mb-2">
+                        <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                      Iniciar sesión
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Tab>
+            <Tab eventKey="register" title="Registrarse">
+              <Row>
+                <Col md={6} className='formRegister'>
+                  <Form>
+                    <Form.Group controlId="formBasicName">
+                      <Form.Label>Nombre</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Ingrese su nombre"
+                      />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicLastName">
+                      <Form.Label>Apellido</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Ingrese su apellido"
+                      />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPhone">
+                      <Form.Label>Número de Teléfono</Form.Label>
+                      <Form.Control
+                        type="tel"
+                        placeholder="Ingrese su número de teléfono"
+                      />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                      <Form.Text className="text-muted">
+                        <p>* La contraseña debe tener al menos 6 caracteres. Debe
+                        incluir minúsculas, mayúsculas y al menos un número.</p>
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox" className="custom-checkbox">
+                      <Form.Check
+                        type="checkbox"
+                        label="He leído y acepto los Términos y Condiciones y Política de Privacidad"
+                      />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                      Registrarse
+                    </Button>
+                  </Form>
+                </Col>
+                <Col md={6} className="colImg">
+                  <img src={RegisterImg} fluid />
+                </Col>
+              </Row>
+            </Tab>
+          </Tabs>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
 
-export default ModalLogin
+export default ModalLogin;
